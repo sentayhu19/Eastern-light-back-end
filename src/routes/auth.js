66875457cbbf3830990914f6909bc05e8dbffbf0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-24 23:02:25 +0300

    Validations added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,10 @@
 const {Router} = require('express');
 const { getUsers } = require('../controllers.js/auth');
+const { registrationValidation } = require('../validators/auth');
+const { validationMiddleware } = require('../middlewares/validations-middleware');
 const router = Router();
 
 router.get('/get-users', getUsers)
+router.post('/register', registrationValidation, validationMiddleware, register)
 
 module.exports = router;
---
+++ src/validators/auth.js
@@ -0,0 +1,23 @@
+const { check }  = require ( 'express-validator' );
+const db = require ( '../db' );
+
+//password
+const password = check( 'password' ).isLength({min: 6, max: 15}).withMessage('Password must be between 6 and 15 characters');
+
+//email
+const email = check('email'.isEmail().withMessage('Email is not valid'));
+
+//check if email exists
+
+const emailExists = check('email').custom(async (value) => {
+    const { rows } = await db.query('select * from users where email = $1', [
+value,
+])
+if (rows.length) {
+    throw new Error('Email already exists');
+}
+});
+
+module.exports ={
+    registrationValidation: [email, password, emailExists],
+}
