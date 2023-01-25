41f15ef2fc6ae83afbe77eb481442e78f1ae1d95
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-25 18:09:46 +0300

    Product db created

--- database.sql
+++ database.sql
@@ -6,3 +6,17 @@
     email varchar(255) UNIQUE not null,
     created_at date default CURRENT_DATE
  )
+
+ --Products 
+ CREATE TABLE products (
+      product_id serial primary key,
+      product_name varchar(255) not null,
+      product_description varchar(255) not null,
+      product_brand varchar(255) not null,
+      product_price varchar(255) not null,
+      product_image varchar(255) not null,
+      created_at date default CURRENT_DATE,
+      product_priority INTEGER(255) not null,
+      product_category varchar(255) not null
+)
+ 
