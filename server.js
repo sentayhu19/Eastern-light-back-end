bc7444ecae265409e7dc686ebc97e897de54f780
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-26 18:17:47 +0300

    Fixed the issue with access blocks by CORS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cookieParser = require('cookie-parser')
-const { PORT, CLIENT_ID } = require('./src/constants')
+const { PORT, CLIENT_URL } = require('./src/constants')
 const authRoutes = require('./src/routes/auth')
 const passport = require('passport');
 const cors = require('cors')
@@ -12,11 +12,12 @@ require('./src/middlewares/passport-middleware')
 
 //app starts listening on port 8000
 console.log("PORT: ",PORT)
+app.use(cors({ origin: CLIENT_URL, credentials: true }))
 
 //initialize cookie-parser to allow us access the cookies stored in the browser.
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({origin: CLIENT_ID, credentials: true}))
+app.use(cors({origin: CLIENT_URL, credentials: true}))
 app.use(passport.initialize())
 
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -5,6 +5,6 @@ console.log("PORT at cons: ",process.env.PORT)
 module.exports = {
     PORT: process.env.PORT,
     SECRET: process.env.SECRET,
-    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_URL: process.env.CLIENT_URL,
     SERVER_URL: process.env.SERVER_URL,
 }
